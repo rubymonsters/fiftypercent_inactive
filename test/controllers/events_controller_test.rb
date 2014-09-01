f083147058be51aafc7db4531d8848f3b161c016
Magdalena <madziaf86@tlen.pl>
2014-09-01 22:07:14 +0200

    creates a new action for events_controller

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,5 +1,5 @@
 class EventsControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should GET index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:events)
@@ -9,7 +9,21 @@ class EventsControllerTest < ActionController::TestCase
     Event.create(name: "Froscon")
     get :index
     assert_select 'ul' do
-        assert_select 'li', 'Froscon'
+      assert_select 'li', 'Froscon'
+    end
+  end
+  
+  test "displays a form for creating event" do
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:event)
+  end
+
+  test "new renders a form" do
+    get :new
+    #puts response.body
+    assert_select 'form' do
+      assert_select 'input'
     end
   end
 end
