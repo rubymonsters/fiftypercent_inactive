e2d62f74a9b215524020c10413b8a15e87b2e4e7
Katalinka <katja.gimpel@gmx.de>
2015-03-02 20:44:36 +0100

    Allow users to create new tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,23 @@
 class TagsController < ApplicationController
 	def show
-		@tag =Tag.find(params[:id])
+		@tag =Tag.find_by!(slug: params[:id])
 	end	
+
+  def new
+    @tag = Tag.new
+  end  
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.valid?
+      @tag.save
+      redirect_to @tag
+    else
+      render :new
+    end
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name, :description, :slug)
+  end
 end	
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,11 @@
 class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :events, :through => :taggings
+
+  validates :name, :slug, presence: true
+
+  def to_param
+    slug
+  end
+
 end
