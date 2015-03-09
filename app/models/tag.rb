f97c67f2a584788099ebbcc3b2699841bcb47286
Katalinka <katja.gimpel@gmx.de>
2015-03-09 21:15:06 +0100

    validations on slug

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,15 @@ class Tag < ActiveRecord::Base
   has_many :taggings
   has_many :events, :through => :taggings
 
-  validates :name, :slug, presence: true
+  validates :name, :slug, presence: true, uniqueness: true
+
+  validate :slug_is_correct 
+
+  def slug_is_correct
+    if slug != CGI.escape(slug) or slug == "new"
+      errors.add(:slug, "Invalid slug")
+    end
+  end
 
   def to_param
     slug
