44ba67461b9ad05ab1e33fea926182732391c8f3
Alina Baciu <alinabaciu87@gmail.com>
2015-03-23 21:20:58 +0100

    Rails 4.2 upgrade WIP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Fiftyprocent::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  # configure the mailer to create full URLs in emails
+  config.action_mailer.default_url_options = {host: 'localhost:3000'}
 end
