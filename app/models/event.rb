e2c87d8bfe0fc717d39aa4b5307ccb8ea6a7053c
Alina Baciu <alinabaciu87@gmail.com>
2015-05-18 21:33:20 +0200

    Added tag field in new view

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   has_many :taggings
   has_many :tags, :through => :taggings
 
-  validates :name, :location, :date, :number_of_women, :number_of_men, :organizer_name, :organizer_url, :percent_women, presence: true
+  validates :name, :location, :date, :number_of_women, :number_of_men, :organizer_name, :organizer_url, :percent_women, :tags, presence: true
 
   def percent_women
   	 #puts "SCHEISSE"
